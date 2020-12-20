06486f7be81ce2955a6aea12f04e8884d3890249
Sankara Gomathy M <38379719+sankaragomathym@users.noreply.github.com>
2020-12-20 23:48:25 +0530

    PWA

---
+++ sw.js
@@ -0,0 +1,42 @@
+const staticCacheName = 'site-static-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/todo.js',
+  '/todo.css',
+  '/images/bg-mobile-dark.jpg',
+  '/images/bg-mobile-light.jpg',
+  '/images/icon-check.svg',
+  '/images/icon-cross.svg',
+  '/images/icon-moon.svg',
+  '/images/icon-sun.svg',
+  'https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@400;700&display=swap',
+];
+// install event
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
+  );
+});
+// activate event
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== staticCacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+// fetch event
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request);
+    })
+  );
+});
--- todo.js
+++ todo.js
@@ -282,3 +282,9 @@ function movedown(fromIndex, toIndex) { // 2 -> 0
         json[i+1] = json[i];
     }
 }
+
+if('serviceWorker' in navigator){
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => console.log('service worker registered'))
+      .catch(err => console.log('service worker not registered', err));
+}
